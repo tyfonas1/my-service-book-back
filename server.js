d3a1f8fca7cb7c2bb39a75c7aa8dc3b59cab7535
tyfonas1 <tyfonas2@gmail.com>
2021-06-24 23:27:51 +0300

    1st sign in sign up

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const http = require("https");
+const http = require("http");
 const app = require("./app");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer(app);
-
+console.log("port", port);
 server.listen(port);
